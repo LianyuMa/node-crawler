09d913b0bdc1a9c3bea53bb82fbe1dbe17288ead
LianyuMa <LianyuMa@users.noreply.github.com>
2016-05-08 21:56:12 -0400

    request replace getURL

--- crawler.js
+++ crawler.js
@@ -1,11 +1,22 @@
-const getURL = require('./lib/geturl');
+const request = require('request');
 
-const url = 'http://www.cbc.ca/news/canada/manitoba/province-of-manitoba-to-overhaul-wetland-strategy-with-new-bill-1.3333333';
+// const getURL = require('./lib/geturl');
 
-getURL(url, (data) => {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log('error');
-  }
+// const url = 'http://www.cbc.ca/news/canada/manitoba/province-of-manitoba-to-overhaul-wetland-strategy-with-new-bill-1.3333333';
+const url = 'https://github.com/integrations';
+
+// getURL(url, (data) => {
+//   if (data) {
+//     console.log(data);
+//   } else {
+//     console.log('error');
+//   }
+// });
+
+request({
+  method: 'GET',
+  url,
+}, (err, response, body) => {
+  if (err) return console.error(err);
+  console.log(body);
 });
